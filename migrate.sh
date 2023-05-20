3c94d78cc925ceb0072f2541208b72ea8b9d35a7
majlitech <hello@majlitech.pl>
2023-05-20 21:36:47 +0200

    Add migration script

--- migrate.sh
+++ migrate.sh
@@ -1,1 +1,6 @@
-mv /etc/clouddns.json /etc/cloudipsyncr
+sudo mv /etc/clouddns.json /etc/cloudipsyncr.json
+sudo rm /usr/bin/cloudipsyncr
+sudo rm /usr/bin/cipsyncr-setup
+echo "You will now need to fetch the latest release of cloudipsyncr"
+echo "This script will download v1.1.3"
+cd ~; mkdir cloudipsyncr; cd cloudipsyncr; wget https://github.com/MajliTech/CloudIPsyncr/releases/download/v1.1.3/linux-x64.zip ; unzip linux-x64; sudo mv cloudipsyncr /usr/bin/; sudo mv cipsyncr-setup /usr/bin; sudo chmod +x /usr/bin/cloudipsyncr; sudo chmod +x cipsyncr-setup;
